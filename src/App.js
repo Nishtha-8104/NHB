78552f5f99ebc389466cbd36e2737d6062d3761e
Om-Patel1705 <122296443+Om-Patel1705@users.noreply.github.com>
2023-12-22 17:52:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function App() {
       {pages.map((page, index) => (
           <div key={index} className="page">
             {page}
- x         </div>
+         </div>
         ))}
       </div>
     </div>
